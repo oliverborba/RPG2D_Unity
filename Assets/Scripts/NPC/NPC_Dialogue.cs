12d808ce38cce2a1091ba190eeb11d8cd918eadb
Lucas Oliveira de Borba <56658658+oliverborba@users.noreply.github.com>
2023-08-09 17:46:03 -0300

    NPC Diálogo
    Implementando primeiros diálogos no NPC

---
+++ Assets/Scripts/NPC/NPC_Dialogue.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Dialogue : MonoBehaviour
+{
+
+    public float dialogueRange;
+    public LayerMask playerLayer;
+
+    public DialogueSettings dialogue;
+
+    bool playerHit;
+
+    private List<string> sentences = new List<string>();
+
+    private void Start()
+    {
+        GetNPCInfo();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerHit)
+        {
+            DialogueControl.instance.Speech(sentences.ToArray());
+        }
+    }
+
+    void GetNPCInfo()
+    {
+        for(int i = 0; i < dialogue.dialogues.Count; i++)
+        {
+            switch (DialogueControl.instance.language)
+            {
+                case DialogueControl.idiom.pt:
+                    sentences.Add(dialogue.dialogues[i].sentence.portuguese);
+                    break;
+
+                case DialogueControl.idiom.eng:
+                    sentences.Add(dialogue.dialogues[i].sentence.english);
+                    break;
+
+                case DialogueControl.idiom.spa:
+                    sentences.Add(dialogue.dialogues[i].sentence.spanish);
+                    break;
+
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        ShowDialogue();
+    }   
+
+    // Update is called once per frame
+    void ShowDialogue()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, dialogueRange, playerLayer);
+
+        if (hit != null)
+        {
+            playerHit = true;
+        }
+        else
+        {
+            playerHit= false;
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, dialogueRange);
+    }
+
+}
